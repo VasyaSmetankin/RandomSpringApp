2a67fd4fe945d2a19d99f68863acb4c4b48874e1
a <a>
2022-11-03 00:28:43 +0300

    Final commit

--- RandomSpringApp/ViewController.swift
+++ RandomSpringApp/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 import SpringAnimation
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet var springPreset: UILabel!
+    @IBOutlet var springCurve: UILabel!
+    @IBOutlet var springForce: UILabel!
+    @IBOutlet var springDuration: UILabel!
+    @IBOutlet var springDelay: UILabel!
+    
+    
     @IBOutlet var springButtonOutlet: SpringButton!
     @IBOutlet var springViewOutlet: SpringView!
+    
+    
     var randomAnimationTypeArray = [
         "pop",
         "slideLeft",
@@ -41,14 +51,41 @@ class ViewController: UIViewController {
         "wobble",
         "swing",
         ]
-    
     var randomCurveTypeArray = [
-        "a"
+        
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack",
     ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         SetupProps()
     }
+    
+    
+    
+    
+    
     private func SetupProps() {
         springViewOutlet.layer.cornerRadius = 10
         springButtonOutlet.layer.cornerRadius = 10
@@ -56,21 +93,29 @@ class ViewController: UIViewController {
         springViewOutlet.backgroundColor = #colorLiteral(red: 0.7159170939, green: 0.7660595397, blue: 0.7039443353, alpha: 1)
 
     }
-    
     @IBAction func springButtonDidTapped(_ sender: SpringButton) {
         
-        springViewOutlet.animation = randomAnimationTypeArray.randomElement() ?? ""
-        springViewOutlet.curve = "easeOutBack"
-        springViewOutlet.force = Double.random(in: 0...1.5)
-        springViewOutlet.duration = Double.random(in: 0.5...1)
-        springViewOutlet.delay = Double.random(in: 0.2...0.8)
+        var springViewOutletType = randomAnimationTypeArray.randomElement() ?? ""
+        var springViewOutletCurve = randomCurveTypeArray.randomElement() ?? ""
+        var springViewOutletForce = Double.random(in: 0...1.5)
+        var springViewOutletDuration = Double.random(in: 0.5...1)
+        var springViewOutletDelay = Double.random(in: 0.2...0.8)
+        
+        springPreset.text = "Preset: \(springViewOutletType)"
+        springCurve.text = "Curve: \(springViewOutletCurve)"
+        springForce.text = "Force: \(NSString(format: "%.2f", springViewOutletForce))"
+        springDuration.text = "Duration: \(NSString(format: "%.2f",springViewOutletDuration))"
+        springDelay.text = "Delay: \(NSString(format: "%.2f",springViewOutletDelay))"
+        
+        springViewOutlet.animation = springViewOutletType
+        springButtonOutlet.setTitle(springViewOutletType, for: .normal)
+        springViewOutlet.curve = springViewOutletCurve
+        
+        springViewOutlet.force = springViewOutletForce
+        springViewOutlet.duration = springViewOutletDuration
+        springViewOutlet.delay = springViewOutletDelay
         springViewOutlet.animate()
-        
-        
-        
-        
     }
-
 
 }
 
