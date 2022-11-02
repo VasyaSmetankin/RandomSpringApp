ddf302ee5aa9f9496edea5727a5f5ee14c2ff337
a <a>
2022-11-02 23:06:59 +0300

    Initial Commit

---
+++ RandomSpringApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomSpringApp
+//
+//  Created by KooK MeeM on 02.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
